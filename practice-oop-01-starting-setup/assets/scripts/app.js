dabea752a3ab78981b1a15e485fba0c6b602bef9
Karen Douglas <kdindependence@gmail.com>
2021-10-15 18:11:17 -0500

    creating project lists and parsing

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,19 @@
+class Tooltip {}
+
+class ProjectItem {}
+
+class ProjectList {
+    constructor(type) {
+           const prjtItems = document.querySelectorAll(`#${type}-projects li`)
+    }
+}
+
+class App {
+    static init() {
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+    }
+
+}
+
+App.init();
